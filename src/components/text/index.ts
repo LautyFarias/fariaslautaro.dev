ac950a62f3f9b77f2e718cb311a022c3907f4645
Lautaro Farias <farias.lautaro002@gmail.com>
2024-03-10 18:07:27 -0300

    fix(Hero): pretty print

--- src/components/text/index.ts
+++ src/components/text/index.ts
@@ -2,3 +2,9 @@ import Paragraph from "@/components/text/Paragraph.astro"
 import Text from "@/components/text/Text.astro"
 
 export { Paragraph, Text }
+
+export enum TextVariant {
+  Primary = "primary",
+  Secondary = "secondary",
+  Tertiary = "tertiary",
+}
