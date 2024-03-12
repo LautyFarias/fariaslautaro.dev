e80f522b13e690761e7969f8316ba11e9279b950
Lautaro Farias <farias.lautaro002@gmail.com>
2024-03-11 21:37:15 -0300

    refactor(text): Move to ui folder

--- src/components/text/index.ts
+++
@@ -1,9 +0,0 @@
-import Paragraph from "@/components/text/Paragraph.astro"
-import Text from "@/components/text/Text.astro"
-
-export { Paragraph, Text }
-
-export enum TextVariant {
-  Primary = "primary",
-  Secondary = "secondary"
-}
---
+++ src/components/ui/text/index.ts
@@ -0,0 +1,9 @@
+import Paragraph from "@/components/ui/text/Paragraph.astro"
+import Text from "@/components/ui/text/Text.astro"
+
+export { Paragraph, Text }
+
+export enum TextVariant {
+  Primary = "primary",
+  Secondary = "secondary"
+}
